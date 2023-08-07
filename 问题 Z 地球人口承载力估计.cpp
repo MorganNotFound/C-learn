ae8d3aa31bd49ac739e0d70dba31df8feb6628b7
Morgan <1037502595@qq.com>
2023-08-07 20:05:20 +0800

    Delete 问题 Z 地球人口承载力估计.cpp

--- 问题 Z 地球人口承载力估计.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main() {
-	int x,a,y,b,r;
-	cin >> x >> a >> y >> b;
-	r = (a*x-b*y)/(a-b);
-	cout << fixed << setprecision(2) << r*1.00;
-    return 0;
-}
